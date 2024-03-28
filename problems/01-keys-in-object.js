26c8da693cfa15698068027b18a021c676679a5f
robweekes <weekes.robert@gmail.com>
2024-03-27 18:34:23 -0700

    done with problems 1 and 2

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -3,7 +3,7 @@ Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
-  test comment
+
 Examples:
 
 let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
@@ -12,9 +12,25 @@ keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
+// function keysInObject(obj) {
+//   console.log(Object.keys(obj));
+//   return Object.keys(obj);
+// }
+
 function keysInObject(obj) {
-  // Your code here
+    let arr = [];
+    for(let keys in obj) {
+        arr.push(keys);
+        console.log(arr);
+    }
+  return Object.keys(obj);
 }
+
+let animals = { dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo' }
+let foods = { apple: 'tart', lemon: 'sour', mango: 'sweet' }
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -1,11 +1,8 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
-
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.values`.
-
-
 Examples:
 
 let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
@@ -14,9 +11,25 @@ valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
 
+// function valuesInObject(obj) {
+//     console.log(Object.values(obj));
+//     return Object.values(obj);
+// }
+
 function valuesInObject(obj) {
-  // Your code here 
+    let arr = [];
+    for(let keys in obj) {
+        console.log(obj[keys])
+        arr.push(obj[keys]);
+        console.log(arr);
+    }
+    return arr;
 }
+
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
